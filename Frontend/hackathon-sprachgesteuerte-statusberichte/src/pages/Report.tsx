1f2328292ccf29ed420d6fb15c6bb3249ad1c701
Maxim <maksimfhmad@gmail.com>
2024-11-28 20:51:52 +0100

    test2

--- Frontend/hackathon-sprachgesteuerte-statusberichte/src/pages/Report.tsx
+++ Frontend/hackathon-sprachgesteuerte-statusberichte/src/pages/Report.tsx
@@ -22,9 +22,22 @@ export default function Report({ reportNumber }: Props) {
           <div className="grid grid-cols-3">
             <div>Mock-Report-1</div>
             <div>Dornbirn</div>
-            <button>
-              <div className="flex justify-end">Details anzeigen</div>
-            </button>
+            <div className="flex justify-end items-center ">
+              <button className="flex justify-between gap-2 items-center hover:bg-[#F4F8FB] rounded-2xl px-3">
+                <div>Details anzeigen</div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="12"
+                  fill="#121212"
+                  viewBox="0 0 256 256"
+                  aria-hidden="true"
+                  focusable="false"
+                >
+                  <path d="M184.49,136.49l-80,80a12,12,0,0,1-17-17L159,128,87.51,56.49a12,12,0,1,1,17-17l80,80A12,12,0,0,1,184.49,136.49Z"></path>
+                </svg>
+              </button>
+            </div>
           </div>
           <div className="h-[1px] bg-[#9B9B9B] my-5"></div>
         </div>
